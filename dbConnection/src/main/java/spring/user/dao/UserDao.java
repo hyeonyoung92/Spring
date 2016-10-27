ba50c1c831932ff7c2f99503c9eba59ce4635f1d
hyeonyoung92 <hyeonyounglee@gmail.com>
2016-10-27 17:06:19 +0900

    add the UserClass and UserDaoClass

---
+++ dbConnection/src/main/java/spring/user/dao/UserDao.java
@@ -0,0 +1,64 @@
+package spring.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import spring.user.domain.User;
+
+public class UserDao {
+
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+		Connection con = DriverManager.getConnection("jdbc:ucanaccess://C:\\users.mdb");
+
+		PreparedStatement ps = con.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+
+		ps.executeUpdate();
+
+		ps.close();
+		con.close();
+	}
+
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+		Connection con = DriverManager.getConnection("jdbc:ucanaccess://C:\\users.mdb");
+
+		PreparedStatement ps = con.prepareStatement("select * from users where id = ?");
+		ps.setString(1, id);
+
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setId(rs.getString("name"));
+		user.setId(rs.getString("password"));
+
+		rs.close();
+		ps.close();
+		con.close();
+
+		return user;
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao dao = new UserDao();
+		User user = new User();
+		user.setId("weelde");
+		user.setName("«Ù≥Á¿Ã");
+		user.setPassword("racheal");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId()+"µÓ∑œOK");
+		
+		
+	}
+
+}
